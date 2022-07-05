59bbebe037a88a21cb9ed8cc03623e3bb0b3e803
dn070017 <dn070017@gmail.com>
2022-07-05 13:08:15 +0200

    add LogTransform.py; fix bugs for NormalizeLibrarySize.py (accidentally turn the sparse matrix to dense matrix)

--- cavachon/preprocess/NormalizeLibrarySize.py
+++ cavachon/preprocess/NormalizeLibrarySize.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from cavachon.preprocess.PreprocessStep import PreprocessStep
 
+import numpy as np
+
 class NormalizeLibrarySize(PreprocessStep):
 
   def __init__(self, name, kwargs):
@@ -9,10 +11,16 @@ class NormalizeLibrarySize(PreprocessStep):
   def execute(self, modality: Modality) -> None:
     field = self.kwargs.get('field', None)
     if field is not None and field in modality.adata.obs.columns:
-      libsize = modality.adata.obs[field].values
+      libsize = np.reshape(modality.adata.obs[field].values, (-1, 1))
     else:
       libsize = modality.adata.X.sum(axis=1)
       modality.adata.obs['libsize'] = libsize
-    modality.adata.X  = modality.adata.X / libsize
+    
+    coo_matrix = modality.adata.X.tocoo()
+    scale_fn = np.vectorize(lambda x: libsize[x, 0])
+    scale_factor = scale_fn(coo_matrix.row)
+    min_nonzero = np.min(modality.adata.X[modality.adata.X != 0])
+    normalized_x = modality.adata.X[modality.adata.X >= min_nonzero] / scale_factor
+    modality.adata.X[modality.adata.X >= min_nonzero] = normalized_x
     
     return
--- cavachon/preprocess/__init__.py
+++ cavachon/preprocess/__init__.py
@@ -2,5 +2,6 @@ from .Binarize import Binarize
 from .FilterCells import FilterCells
 from .FilterGenes import FilterGenes
 from .FilterQC import FilterQC
+from .LogTransform import LogTransform
 from .NormalizeLibrarySize import NormalizeLibrarySize
 from .PreprocessStep import PreprocessStep
