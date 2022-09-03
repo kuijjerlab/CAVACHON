b03ed9848fb91ce268bbf717d044d41ab8499aea
dn070017 <dn070017@gmail.com>
2022-09-03 22:33:53 +0200

    add docstring

--- cavachon/modules/preprocessors/Preprocessor.py
+++ cavachon/modules/preprocessors/Preprocessor.py
@@ -1,11 +1,54 @@
-#%%
 from cavachon.environment.Constants import Constants
 from cavachon.utils.ReflectionHandler import ReflectionHandler
-from typing import List, Mapping
+from typing import Any, List, Mapping
 import tensorflow as tf
 
 class Preprocessor(tf.keras.Model):
-  def __init__(self, inputs, outputs, name, matrix_key):
+  """Preprocessor
+
+  Preprocessor module used in components. The batch data from 
+  tf.data.Dataset will be first put into this preprocessor, which will
+  apply corresponding modifiers based on the specified distribtion
+  in each modality. Afterwards, it will concatenate the processed
+  data into one single tf.Tensor stored in the outputs Mapping. This 
+  base module is implemented using Tensorflow functional API.
+
+  Attributes
+  ----------
+  matrix_key: Any
+      the key used to get access to the processed tf.Tensor.
+
+  """
+  def __init__(
+      self,
+      inputs: Mapping[Any, tf.keras.Input],
+      outputs: Mapping[Any, tf.Tensor],
+      name: str = 'preprocessor',
+      matrix_key: Any = (Constants.TENSOR_NAME_X, )):
+    """Constructor for Preprocessor. Should not be called directly most
+    of the time. Please use make() to create the model.
+
+    Arguments
+    ---------
+    inputs: Mapping[Any, tf.keras.Input]): 
+        inputs for building tf.keras.Model using Tensorflow functional 
+        API. Expect to have key (modality_name, Constants.TENSOR_NAME_X)
+        by defaults, and optionally expect 
+        (modality_name, Constants.LIBSIZE) for modality that needs to 
+        be scaling by library size.
+    
+    outputs: Mapping[Any, tf.Tensor]):
+        outputs for building tf.keras.Model using Tensorflow functional
+        API. Have key (self.matrix_key)
+    
+    name: str, optional:
+        Name for the tensorflow model. Defaults to 'preprocessor'.
+
+    matrix_key: Any, optional 
+        key to access the processed tf.Tensor. Defaults to 
+        (Constants.TENSOR_NAME_X, ).
+
+    """
     super().__init__(inputs=inputs, outputs=outputs, name=name)
     self.matrix_key = matrix_key
 
@@ -18,6 +61,36 @@ class Preprocessor(tf.keras.Model):
       n_dims: int = 1024,
       name: str = 'preprocessor',
       **kwargs) -> tf.keras.Model:
+    """Make the tf.keras.Model using the functional API of Tensorflow.
+        
+    Parameters
+    ----------
+    modality_names: List[str]
+        names for the modality needs to be processed.
+    
+    distribution_names: Mapping[str, str]
+        distribution for each modality. The key should be the names for
+        each modality and the values are the names for corresponding
+        distributions.
+
+    n_vars: Mapping[str, int]
+        number of variables for the inputs Tensors. The key should be 
+        the names for each modality and the values number of feature
+        in the last dimension of the modality Tensor.
+
+    n_dims: int, optional
+        number of dimension to reduce to before concatenation. Defaults
+        to 1024.
+
+    name: str, optional
+        Name for the tensorflow model. Defaults to 'preprocessor'.
+
+    Returns
+    -------
+    tf.keras.Model
+        Created model using Tensorflow functional API.
+    
+    """
     
     inputs = dict()
     outputs = dict()
--- cavachon/modules/preprocessors/modifiers/IndependentBernoulli.py
+++ cavachon/modules/preprocessors/modifiers/IndependentBernoulli.py
@@ -7,7 +7,42 @@ import functools
 import tensorflow as tf
 
 class IndependentBernoulli(tf.keras.Model):
+  """IndependentBernoulli
+
+  Modifiers for the modality which is IndependentBernoulli distribution.
+  The instance will be used before calling the tf.keras.Model. Note that
+  this will not change the data in the DataLoader.dataset which
+  dataloader.modifiers.IndependentBernoulli does.
+
+  Attributes
+  ----------
+  modality_names: str
+      modality name.
+
+  modality_key: Tuple[str]
+      the key used to access the mapping of data created from 
+      tf.data.Dataset. Defaults to (modality_name, 'matrix').
+
+  modifiers: List[tf.keras.layers.Layer]
+      list of modifiers that will be applied to the data created from 
+      tf.data.Dataset. Defaults to [ToDense, Binarize].
+  
+  See Also
+  --------
+  dataloader.modifiers.IndependentBernoulli
+      similar modifier but used for DataLoader.dataset
+  
+  """
   def __init__(self, modality_name):
+    """Constructor for IndependentBernoulli (modifier for 
+    tf.data.Dataset)
+
+    Parameters
+    ----------
+    modality_name: str 
+        the name of modality that needs to be processed.
+        
+    """
     super().__init__()
     self.modality_name = modality_name
     self.modality_key = (modality_name, Constants.TENSOR_NAME_X)
@@ -17,5 +52,24 @@ class IndependentBernoulli(tf.keras.Model):
     ]
   
   def call(self, inputs, training=None, mask=None):
+    """Processed the data created from tf.data.Dataset.
+
+    Parameters
+    ----------
+    inputs: 
+        Mapping of tf.Tensor, where the keys contain self.modality_key. 
+
+    training: bool, optional
+        Not used (kept for tf.keras.Model API). 
+
+    mask: tf.Tensor, optional
+        Not used (kept for tf.keras.Model API). 
+
+    Returns
+    -------
+    Mapping[Any, tf.Tensor]
+        processed data. 
+
+    """
     modifiers = self.modifiers
     return functools.reduce(lambda x, modifier: modifier(x), modifiers, inputs)
