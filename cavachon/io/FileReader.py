163265b4301504f16f7569919147cb9153778c2a
dn070017 <dn070017@gmail.com>
2022-10-12 15:53:46 +0200

    update to use Constants and has_headers

--- cavachon/io/FileReader.py
+++ cavachon/io/FileReader.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from anndata import AnnData
+from cavachon.environment.Constants import Constants
 from typing import Any, Dict, List, Optional, Union
 from scipy.io import mmread
 from scipy.sparse import csr_matrix, vstack
@@ -37,7 +38,7 @@ class FileReader:
     """
     datadir = config.io.datadir
     config_modality = config.modality.get(modality_name, {})
-    config_sample_list = config_modality.get('samples', [])
+    config_sample_list = config_modality.get(Constants.CONFIG_FIELD_SAMPLE, [])
 
     obs_df_list = []
     var_df_list = []
@@ -46,21 +47,33 @@ class FileReader:
       config_sample = config.sample.get(sample_name)
       config_sample_modality = list(filter(
           lambda x: x.get('name') == modality_name,
-          config_sample.get('modalities'))).pop()
-      sample_description = config_sample.get('description')
+          config_sample.get(Constants.CONFIG_FIELD_MODALITY))).pop()
+      sample_description = config_sample.get(Constants.CONFIG_FIELD_SAMPLE_DESCRIPTION)
+      
+      has_headers = config_sample_modality.get(Constants.CONFIG_FIELD_SAMPLE_MODALITY_HAS_HEADERS)
+      field_obs_colnames = Constants.CONFIG_FIELD_SAMPLE_MODALITY_OBS_COLNAMES
+      obs_colnames = config_sample_modality[field_obs_colnames] if not has_headers else None
       obs_df = FileReader.read_table(
-          filename=os.path.join(datadir, config_sample_modality['barcodes']),
+          filename=os.path.join(
+              datadir, 
+              config_sample_modality[Constants.CONFIG_FIELD_SAMPLE_MODALITY_OBS]),
           name=modality_name,
-          colnames=config_sample_modality['barcodes_colnames'])
+          colnames=obs_colnames)
       obs_df['Sample'] = sample_name
       obs_df['Description'] = sample_description
 
+      field_var_colnames = Constants.CONFIG_FIELD_SAMPLE_MODALITY_VAR_COLNAMES
+      var_colnames = config_sample_modality[field_var_colnames] if not has_headers else None
       var_df = FileReader.read_table(
-          filename=os.path.join(datadir, config_sample_modality['features']),
+          filename=os.path.join(
+              datadir, 
+              config_sample_modality[Constants.CONFIG_FIELD_SAMPLE_MODALITY_VAR]),
           name=modality_name,
-          colnames=config_sample_modality['features_colnames'])
+          colnames=var_colnames)
       
-      matrix = FileReader.read_mtx(os.path.join(datadir, config_sample_modality['matrix']))
+      matrix = FileReader.read_mtx(os.path.join(
+          datadir, 
+          config_sample_modality[Constants.CONFIG_FIELD_SAMPLE_MODALITY_MTX]))
       obs_df_list.append(obs_df)
       var_df_list.append(var_df)
       matrix_list.append(matrix)
@@ -79,7 +92,7 @@ class FileReader:
   def read_table(
       filename: str,
       name: str,
-      colnames: List[str],
+      colnames: Optional[List[str]] = None,
       delimiter: str = '\t',
       index_col: Optional[Union[str, int]] = 0,
       **kwargs) -> pd.DataFrame:
@@ -94,9 +107,9 @@ class FileReader:
         the header for the name (name:index_name) for the index of the 
         DataFrame.
       
-    colnames: List[str]
+    colnames: Optional[List[str]]
         the column names of the DataFrame (will replace the original 
-        one).
+        one if provided).
       
     delimiter: str, optional
         delimiter for the file. Defaults to '\t'.
@@ -116,11 +129,12 @@ class FileReader:
     """
     filename = os.path.realpath(filename)
     df = pd.read_csv(filename, sep=delimiter, header=None, **kwargs)
-    df.columns = colnames
-    if not isinstance(index_col, int):
-      index_col = colnames.index(index_col)
-    index_name = colnames[int(index_col)]
-    df.index = pd.Index(df[index_name], name=f"{name}:{index_name}")
+    if colnames is not None:
+      df.columns = colnames
+      if not isinstance(index_col, int):
+        index_col = colnames.index(index_col)
+      index_name = colnames[int(index_col)]
+      df.index = pd.Index(df[index_name], name=f"{name}:{index_name}")
 
     return df
 
