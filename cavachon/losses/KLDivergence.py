fix the bug for incorrect loss_weights, fix training mode in batch normalization when setting component.trainable = False

cavachon/losses/KLDivergence.py
@@ -11,12 +11,16 @@ class KLDivergence(tf.keras.losses.Loss):
   logpx_z + 𝚺_j𝚺_y[py_z(logpz_y + logpy)] - 𝚺_j[logqz_x] - 
   𝚺_j𝚺_y[py_z(logpc_z)] 
   """
-  def __init__(self, name: str = 'kl_divergence', **kwargs):
+  def __init__(self, weight: float = 1.0, name: str = 'kl_divergence', **kwargs):
     """Constructor for KLDivergence
 
     Parameters
     ----------
-    name: str.
+    weight: float, optional
+        the scaling factor for the loss. The output will be 
+        weight * loss. Defaults to 1.0.
+
+    name: str, optional
         name for the tf.keras.losses.Loss (will be used when reporting
         the loss during training_step in Component and Model).
         Defaults to 'kl_divergence'.
@@ -25,6 +29,7 @@ class KLDivergence(tf.keras.losses.Loss):
         additional parameters for tf.keras.losses.Loss
 
     """
+    self.weight = weight
     super().__init__(name=name, reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE, **kwargs)
 
   def call(self, y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
@@ -116,4 +121,4 @@ class KLDivergence(tf.keras.losses.Loss):
     kl_divergence = -py_z_logpz_y - py_z_logpy + py_z_logpy_z + logqz_x
     kl_divergence = tf.where(kl_divergence < 0, tf.zeros_like(kl_divergence), kl_divergence)
 
-    return kl_divergence
+    return self.weight * kl_divergence

cavachon/model/Model.py
@@ -406,8 +406,8 @@ class Model(tf.keras.Model):
       self,
       **kwargs) -> None:
     """Compile the model before training. Note that the 'metrics' will 
-    be ignored in Model due to some unexpected behaviour for Tensorflow. 
-    The 'loss' will be setup automatically if not provided.
+    be ignored in Model becaus of the incompatibility with Tensorflow
+    API. The 'loss' will be setup automatically if not provided.
 
     Parameters
     ----------
@@ -415,6 +415,9 @@ class Model(tf.keras.Model):
         Additional parameters used to compile the model.
 
     """
+    loss_weights = kwargs.get('loss_weights', dict())
+    kwargs.pop('loss_weights', None)
+
     if 'loss' not in kwargs:
       loss = dict()
       for component_config in self.component_configs:
@@ -422,7 +425,7 @@ class Model(tf.keras.Model):
         kl_divergence_name = f'{component_name}/{Constants.MODEL_LOSS_KL_POSTFIX}'
         loss.setdefault(
             kl_divergence_name,
-            KLDivergence(name=kl_divergence_name))
+            KLDivergence(loss_weights.get(kl_divergence_name, 1.0), name=kl_divergence_name))
 
         for modality_name in component_config.get('modality_names'):
           nldl_name = f'{component_name}/{modality_name}/{Constants.MODEL_LOSS_DATA_POSTFIX}'
@@ -432,6 +435,7 @@ class Model(tf.keras.Model):
               nldl_name,
               NegativeLogDataLikelihood(
                   distribution_names.get(modality_name),
+                  loss_weights.get(nldl_name, 1.0),
                   name=nldl_name))
       kwargs.setdefault('loss', loss)
     else:
@@ -442,9 +446,9 @@ class Model(tf.keras.Model):
     
     if 'metrics' in kwargs:
       message = ''.join((
-        f'Due to the unexpected behaviour with compiled_loss when providing less number ',
-        f'of custom losses in Tensorflow 2.8.0. The custom metrics provided to compile() in',
-        f'{self.__class__.__name__} will be ignored.'))
+        f'Due to the incompatibility of the compiled_loss with Tensorflow 2.8.1 (as the model ',
+        f'requires outputs from multiple components to compute the KLDivergence), The custom ',
+        f'metrics provided to compile() in {self.__class__.__name__} will be ignored.'))
       warnings.warn(message, RuntimeWarning)
       kwargs.pop('metrics')
 

cavachon/modules/components/Component.py
@@ -35,9 +35,20 @@ class Component(tf.keras.Model):
       distribution names. This is used to automatically find the 
       parameterizer in modules.parameterizers for data distributions.
   
+  encoder: tf.keras.Model
+      encoder neural network.
+
   z_prior_parameterizer: tf.keras.layers.Layer
       parameterizer used for the priors in latent distributions. Used 
       when computing the KLDivergence.
+
+  hierarchical_encoder: tf.karas.Model
+      hierarchical encoder used to encode z_hat hierarchically through 
+      the dependency between components.
+  
+  decoders: Mapping[str, tf.keras.Model]
+      decoder neural networks. The keys are the name of the modality,
+      the values are the corresponding decoder neural network.
       
   name: str, optional
       Name for the tensorflow model. Defaults to 'component'.
@@ -48,9 +59,10 @@ class Component(tf.keras.Model):
     outputs: Mapping[Any, tf.Tensor],
     modality_names: List[Any],
     distribution_names: Mapping[str, str],
+    encoder: tf.keras.Model,
     z_prior_parameterizer: tf.keras.layers.Layer,
     hierarchical_encoder: tf.keras.Model,
-    decoders: tf.keras.Model,
+    decoders: Mapping[str, tf.keras.Model],
     name: str = 'component',
     **kwargs):
     """Constructor for Component. Should not be called directly most of 
@@ -78,9 +90,20 @@ class Component(tf.keras.Model):
         automatically find the parameterizer in modules.parameterizers
         for data distributions.
   
+    encoder: tf.keras.Model
+        encoder neural network.
+
     z_prior_parameterizer: tf.keras.layers.Layer
         parameterizer used for the priors in latent distributions. Used 
         when computing the KLDivergence.
+
+    hierarchical_encoder: tf.karas.Model
+        hierarchical encoder used to encode z_hat hierarchically through 
+        the dependency between components.
+    
+    decoders: Mapping[str, tf.keras.Model]
+        decoder neural networks. The keys are the name of the modality,
+        the values are the corresponding decoder neural network.
 
     name: str, optional:
         Name for the tensorflow model. Defaults to 'component'.
@@ -92,6 +115,7 @@ class Component(tf.keras.Model):
     super().__init__(inputs=inputs, outputs=outputs, name=name)
     self.modality_names = modality_names
     self.distribution_names = distribution_names
+    self.encoder = encoder
     self.z_prior_parameterizer = z_prior_parameterizer
     self.hierarchical_encoder = hierarchical_encoder
     self.decoders = decoders
@@ -687,6 +711,7 @@ class Component(tf.keras.Model):
         outputs=outputs,
         modality_names=modality_names,
         distribution_names=distribution_names,
+        encoder=encoder,
         z_prior_parameterizer=z_prior_parameterizer,
         hierarchical_encoder=hierarchical_encoder,
         decoders=decoders,
@@ -697,8 +722,8 @@ class Component(tf.keras.Model):
       self,
       **kwargs) -> None:
     """Compile the model before training. Note that the 'metrics' will 
-    be ignored in Component due to some unexpected behaviour for 
-    Tensorflow. The 'loss' will be setup automatically if not provided.
+    be ignored in Model becaus of the incompatibility with Tensorflow
+    API. The 'loss' will be setup automatically if not provided.
 
     Parameters
     ----------
@@ -706,19 +731,23 @@ class Component(tf.keras.Model):
         Additional parameters used to compile the model.
 
     """
+    loss_weights = kwargs.get('loss_weights', dict())
+    kwargs.pop('loss_weights', None)
+    
     if 'loss' not in kwargs:
       loss = OrderedDict()
       kl_divergence_name = Constants.MODEL_LOSS_KL_POSTFIX
       loss.setdefault(
           kl_divergence_name,
-          KLDivergence(name=kl_divergence_name))
+          KLDivergence(loss_weights.get(kl_divergence_name, 1.0), name=kl_divergence_name))
       for modality_name in self.modality_names:
-        negative_log_data_likelihood_name = f'{modality_name}/{Constants.MODEL_LOSS_DATA_POSTFIX}'
+        nldl_name = f'{modality_name}/{Constants.MODEL_LOSS_DATA_POSTFIX}'
         loss.setdefault(
-            negative_log_data_likelihood_name,
+            nldl_name,
             NegativeLogDataLikelihood(
                 self.distribution_names.get(modality_name),
-                name=negative_log_data_likelihood_name))
+                loss_weights.get(nldl_name, 1.0),
+                name=nldl_name))
       kwargs.setdefault('loss', loss)
     else:
       message = ''.join((
@@ -728,9 +757,9 @@ class Component(tf.keras.Model):
 
     if 'metrics' in kwargs:
       message = ''.join((
-        f'Due to the unexpected behaviour with compiled_loss when providing less number ',
-        f'of custom losses in Tensorflow 2.8.0. The custom metrics provided to compile() in',
-        f'{self.__class__.__name__} will be ignored.'))
+        f'Due to the incompatibility of the compiled_loss with Tensorflow 2.8.1 (as the model ',
+        f'requires outputs from multiple components to compute the KLDivergence), The custom ',
+        f'metrics provided to compile() in {self.__class__.__name__} will be ignored.'))
       warnings.warn(message, RuntimeWarning)
       kwargs.pop('metrics')
 

cavachon/workflow/Workflow.py
@@ -11,6 +11,7 @@ from cavachon.scheduler.SequentialTrainingScheduler import SequentialTrainingSch
 from typing import List, MutableMapping, Optional
 
 import anndata
+import muon as mu
 import os
 import tensorflow as tf
 
@@ -23,6 +24,9 @@ class Workflow():
   ---------
   config: Config
       configuration for the workflow.
+
+  mdata: mu.MuData
+      the multi-modality data.
   
   dataloader: DataLoader
       data loader used to create tf.data.Dataset from the input data.
@@ -51,6 +55,7 @@ class Workflow():
 
     """
     self.config: Config = Config(filename)
+    self.mdata: Optional[mu.Mudata] = None
     self.dataloader: Optional[DataLoader] = None
     self.anndata_filters: AnnDataFilterHandler = AnnDataFilterHandler.from_config(self.config)
     self.model: Optional[Model] = None
@@ -69,10 +74,14 @@ class Workflow():
 
   def run(self) -> None:
     """Run the specified workflow configured in self.config"""
-    self.read_modalities()
-    self.filter_anndata()
-    self.dataloader = DataLoader(self.multi_modalities)
+    adata_dict = Workflow.read_modalities(self.config)
+    self.anndata_filters(adata_dict)
+    self.mdata = MultiModality(adata_dict)
+    self.update_config_nvars()
+
+    self.dataloader = DataLoader(self.mdata)
     self.update_nvars_batch_effect()
+
     self.model = Model.make(self.config.components)
     self.train_scheduler = SequentialTrainingScheduler(self.model, self.optimizer)
     batch_size = self.config.dataset.get(Constants.CONFIG_FIELD_MODEL_DATASET_BATCHSIZE)
@@ -80,46 +89,50 @@ class Workflow():
     self.train_history = self.train_scheduler.fit(
         self.dataloader.dataset.batch(batch_size),
         epochs=max_epochs)
-    self.outputs = self.model.predict(self.multi_modalities, batch_size=batch_size)
+    self.outputs = self.model.predict(self.mdata, batch_size=batch_size)
+    self.model.save_weights(
+        os.path.join(f'{self.config.io.outdir}/', 'logging/', self.model.name))
 
     return
   
-  def read_modalities(self) -> None:
-    """Read the modality files and stored the data in self.modalities"""
-    self.modalities = dict()
-    for modality_name in self.config.modality_names:
-      modality_config = self.config.modality[modality_name]
+  @staticmethod
+  def read_modalities(config: Config) -> MutableMapping[str, anndata.AnnData]:
+    """Read the modality files from the configuration.
+
+    Returns
+    -------
+    MutableMapping[str, anndata.AnnData]
+        the keys are the names of the modality, the values are the 
+        corresponding AnnData.
+
+    """
+    modalities = dict()
+    for modality_name in config.modality_names:
+      modality_config = config.modality[modality_name]
       h5ad = modality_config.get(Constants.CONFIG_FIELD_MODALITY_H5AD)
       if h5ad:
-        self.modalities.setdefault(
+        modalities.setdefault(
             modality_name,
-            anndata.read_h5ad(os.path.join(self.config.io.datadir, h5ad)))
+            anndata.read_h5ad(os.path.join(config.io.datadir, h5ad)))
       else:
-        self.modalities.setdefault(
+        modalities.setdefault(
             modality_name,
             Modality(
-                FileReader.read_multiomics_data(self.config, modality_name), 
+                FileReader.read_multiomics_data(config, modality_name), 
                 name=modality_name,
                 distribution_name=modality_config.get(Constants.CONFIG_FIELD_MODALITY_DIST),
                 modality_type=modality_config.get(Constants.CONFIG_FIELD_MODALITY_TYPE)))
-    return
-
-  def filter_anndata(self) -> None:
-    """Filter the AnnData as configured in self.config.filter"""
-    self.anndata_filters(self.modalities)
-    self.multi_modalities = MultiModality(self.modalities)
-    self.update_config_nvars()
-
-    return
+    
+    return modalities
   
   def update_config_nvars(self) -> None:
-    """Update the number of variables after filtering AnnData"""
+    """Update the number of variables in the config after filtering AnnData"""
     processed_component_configs = list()
     for component_config in self.config.components:
       component_vars = dict()
       for modality_name in component_config.get(Constants.CONFIG_FIELD_COMPONENT_MODALITY_NAMES):
-        modality = self.modalities.get(modality_name)
-        component_vars.setdefault(modality_name, modality.n_vars)
+        component_vars.setdefault(modality_name, self.mdata[modality_name].n_vars)
+
       component_config[Constants.CONFIG_FIELD_COMPONENT_N_VARS] = component_vars
       processed_component_configs.append(component_config)
     