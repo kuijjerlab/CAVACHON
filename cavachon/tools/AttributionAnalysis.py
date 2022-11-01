721ff01e6b49cf0b5056fdd932e53209d402cf7f
dn070017 <dn070017@gmail.com>
2022-11-01 22:18:40 +0100

    add docstring for compute_integrated_gradient

--- cavachon/tools/AttributionAnalysis.py
+++ cavachon/tools/AttributionAnalysis.py
@@ -4,7 +4,7 @@ from cavachon.tools.DifferentialAnalysis import DifferentialAnalysis
 from cavachon.utils.ReflectionHandler import ReflectionHandler
 from cavachon.utils.TensorUtils import TensorUtils
 from copy import deepcopy
-from typing import Sequence, List, Union
+from typing import Set, Sequence, List, Optional, Union
 from tqdm import tqdm
 
 import muon as mu
@@ -51,7 +51,7 @@ class AttributionAnalysis:
       modality: str,
       exclude_components: List[str],
       batch_size: int = 128
-  ) -> np.array:
+  ) -> np.ndarray:
     """Compute the x - x_baseline in the integrated gradients. The 
     baseline is the mean of the outputs modality from the component
     without using the latent representation z of the exclude component.
@@ -59,7 +59,7 @@ class AttributionAnalysis:
     Parameters
     ----------
     component : str
-        the outputs of the component to used.
+        the outputs of which component to used.
     
     modality : str
         which modality of the outputs of the component to used.
@@ -73,7 +73,7 @@ class AttributionAnalysis:
 
     Returns
     -------
-    np.array
+    np.ndarray
         x - x_baseline in the integrated gradients.
 
     """
@@ -109,8 +109,32 @@ class AttributionAnalysis:
       modality: str,
       target_component: str, 
       steps: int = 10,
-      batch_size: int = 128,
-      sampling: bool = True) -> np.array:
+      batch_size: int = 128) -> np.ndarray:
+    """Compute the integrated gradients of ∂rho_m/∂z_m.
+
+    Parameters
+    ----------
+    component: str
+        the outputs of which component to used.
+
+    modality: str
+        which modality of the outputs of the component to used.
+
+    target_component: str
+        the latent representation of which component to used.
+
+    steps: int, optional
+        steps in integrated gradients. Defaults to 10.
+
+    batch_size: int, optional
+        batch size used for the forward pass. Defaults to 128
+
+    Returns
+    -------
+    np.ndarray
+        integrated gradients of ∂rho_m/∂z_m.
+
+    """
 
     component_network = self.model.components[component]
     integrated_gradients = []
@@ -119,15 +143,15 @@ class AttributionAnalysis:
         component = component,
         modality = modality,
         exclude_components = [target_component],
+        training = False,
         batch_size = batch_size)
-      
+    
     dataloader = DataLoader(self.mdata, batch_size=batch_size)
     delta_x_split = TensorUtils.split(delta_x, batch_size=batch_size)
 
-    for batch, batch_delta_x in zip(dataloader, delta_x_split):
-      outputs = self.model(batch, training=sampling)
+    for batch, batch_delta_x in tqdm(zip(dataloader, delta_x_split)):
+      outputs = self.model(batch, training=False)
       
-      # need to store conditioned_on_z, and conditioned_on_z_hat to mdata (or to Model)
       unintegrated_gradients_batch = [] 
       for alpha in [x / steps for x in range(steps + 1)]:
         with tf.GradientTape() as tape:
